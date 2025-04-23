1cebfe21b12e546e194c7c7b40ce525022086619
RX67-RX67 <rx67@georgetown.edu>
2025-04-23 11:26:26 -0400

    have problem with connected scatter plot

--- code/data_cleaning/data_cleaning_efficiency_cost.py
+++ code/data_cleaning/data_cleaning_efficiency_cost.py
@@ -15,6 +15,22 @@ df = df[keep] # column
 
 # df.to_csv("data/Processed/efficiency_productivity/under_process.csv", index = False)
 
+target_measures = ["Labor productivity", "Unit labor costs"]
+df = df[df['Measure'].isin(target_measures) & (df['Units'] == "Index (2017=100)")]
+
+quarter_cols = [c for c in df.columns if re.match(r'^\d{4}\sQ[1-4]$', c)]
+
+df_long = df.melt(id_vars=['Measure'], value_vars=quarter_cols, var_name='Quarter', value_name='Value')
+df_conn = df_long.pivot(index='Quarter', columns='Measure', values='Value').reset_index()
+
+df_conn['Quarter_clean'] = df_conn['Quarter'].str.replace(r'\s+', '', regex=True)
+df_conn['Date'] = pd.PeriodIndex(df_conn['Quarter_clean'], freq='Q').to_timestamp()
+df_conn = df_conn[['Date', 'Labor productivity', 'Unit labor costs']].sort_values('Date').reset_index(drop=True)
+
+df_conn.to_csv("data/Processed/efficiency_productivity/efficiency_productivity.csv")
+
+
+
 
 
 
